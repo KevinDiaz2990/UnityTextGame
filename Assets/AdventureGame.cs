07e4fa8029ce0676b5e2a31d1bc73a1bebada7eb
Kevin Diaz <kevindiaz2990@gmail.com>
2018-09-03 17:52:59 -0400

    Initial commit with all the assets thus far

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+    [SerializeField] Text textcomponent;
+    [SerializeField] State startingState;
+    
+    
+     
+
+    State state;
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textcomponent.text = state.GetStateStory();
+       
+	}
+	
+ 
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.getNextStates(); //var will be interpreted as State[] by the C# compiler
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textcomponent.text = state.GetStateStory();
+    }
+}
